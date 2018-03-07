ded0e3df9049568b66fd0ede3ecfc74f52b0fcaa
Suzanne Smigielski <ssmigiel571@gmail.com>
2018-03-07 19:36:23 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,11 @@
+required_relative
+'../current_age_for_birth_year.rb'
+
+describe "current_age_for_birth_year method" do
+    it "returns the age of a person base on the year of birth" do
+    age_of_person =
+    current_age_for_birth_year(1984)
+
+    expect(age_of_person).to eq(19)
+    end
+end
